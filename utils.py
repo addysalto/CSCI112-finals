303615b6a6569dbafe4f02be8dfff63a2dfb5668
addysalto <125242948+addysalto@users.noreply.github.com>
2024-12-03 16:40:18 +0800

    Update utils.py to establish connection to DB in atlas

--- utils.py
+++ utils.py
@@ -1,1 +1,12 @@
-# Create mongoDB connection here
+# Establish the connection to the database
+
+from pymongo import MongoClient
+from datetime import datetime
+
+def openConnection():
+   url = 'mongodb+srv://grp5:Ra8b2hkQQ7dFiFUv@grp5-csci112-finalproj.n88pt.mongodb.net/' 
+   conn = MongoClient(url)
+   return conn 
+
+def closeConnection(conn):
+    conn.close()
